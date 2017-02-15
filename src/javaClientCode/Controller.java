c989d119ddc35e3ed5d68cb3e600be6c97b6dbce
myris3 <fritzolavmyrvang@hotmail.com>
2017-02-15 15:45:49 +0100

    Ikke masse nytt

--- src/javaClientCode/Controller.java
+++ src/javaClientCode/Controller.java
@@ -16,6 +16,8 @@ public class Controller { //Connects all different components, lots of control n
 	
 	public static void main(String args[]){
 		
+		
 		System.out.println("Asking questions");	
+		
 	}
 }
