9d37353ce9b208f281efc0c3bee6b3326f69dc6e
myris3 <fritzolavmyrvang@hotmail.com>
2017-03-07 14:01:22 +0100

    ServerClient mods
    Begynenr å ligne på noe robust protokol

--- src/javaClientCode/ServerClient.java
+++ src/javaClientCode/ServerClient.java
@@ -50,28 +50,94 @@ public class ServerClient {
 			parse_info(payload);
 			
 		}
-		catch (Exception e) {
-            System.out.println("Something happened during excecution of send");
-        }  
-	}
-	
-	void receive(){//Fetch user info and status on login maybe
-		try{
-			System.out.println("Received message: " + in.readLine());
+		else if(get_response(payload).equals("message")){
+			parse_message(payload);
+		}
+		else if(get_response(payload).equals("history")){
+			parse_history(payload);
 			
 		}
-		catch (IOException ae){
-			System.out.println("Couldn't get I/O for the connection to host");
+		else if(get_response(payload).equals("error")){
+			parse_error(payload);
+		}
+		else{
+			System.out.println("Bad request");
 		}
 	}
+	String get_response(String payload){
+		return payload.split("\t")[2].split(":")[1];
+	}
+	String get_content(String payload){
+		return payload.split("\t")[3].split(":")[1];
+	}
+	String get_sender(String payload){
+		return payload.split("\t")[1].split(":")[1];
+	}
+	String get_timestamp(String payload){
+		return payload.split("\t")[0].split(":")[1];
+		
+	}
+	void parse_error(String payload){
+		System.out.println(payload);
+	}
+	void parse_message(String payload){
+		System.out.println(payload);
+	}
+	void parse_info(String payload){
+		System.out.println(payload);
+	}
+	void parse_history(String payload){
+		System.out.println(payload);
+	}
+	void listen(){
+		Scanner scanner = new Scanner(System.in); //When scenes are properly set up, all will println to some kind of outputstream
+		while(true){
+			String[] user_input=scanner.nextLine().split(" ");
+			String req=user_input[0];
+			String con = "";
+			for(int x=1;x<user_input.length;x++){
+				con=con+user_input[x];
+				if (x<user_input.length-1){
+					con=con+" ";
+				}
+			}
+			String payload="request:"+req+"\tcontent:"+con;
+			out.println(payload);
+		}
+	}
+	
+	class MessageReceiver implements Runnable{   //Multithreading the sending and responses
+		Thread t;
+		ServerClient client;
+		MessageReceiver(ServerClient client){
+			this.client=client;
+		}
+		public void run(){
+			while (true){
+				try{
+					String payload = client.in.readLine();
+					
+					client.parse(payload);
+				}
+				catch(IOException ee){
+					System.out.println("Server disconnected");
+					break;
+				}
+					
+			}
+		}
+		public void start(){
+			if (this.t==null){
+				this.t= new Thread(this);
+				t.setDaemon(true);
+				t.start();
+			}
+		}
+	}
+	
+	
 	public static void main(String[] args) {
 		ServerClient sender = new ServerClient();
-		Scanner scanner = new Scanner(System.in);
-		while(true){
-			String input = scanner.nextLine();
-			sender.send(""+input);
-			sender.receive();
-		}
 		
 		
 	}
