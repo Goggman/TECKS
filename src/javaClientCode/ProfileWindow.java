f3a1baf006d52137e40bca0d0b84fa81ef032bba
myris3 <fritzolavmyrvang@hotmail.com>
2017-04-05 13:48:16 +0200

    ProfileWIndow in the making

--- src/javaClientCode/ProfileWindow.java
+++ src/javaClientCode/ProfileWindow.java
@@ -16,23 +16,30 @@ import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
-public class ProfileWindow implements Window { //TODO: Make a statistics window, or integrate elements of this window into others
+public class ProfileWindow implements Window { //TODO: Make a profile window, or integrate elements of this window into others
 	Stage stage;
 	GUIController ctrl;
 	ServerClient client;
+	FeedUpdater updater;
 	ProfileWindow(Stage stageIn, GUIController ctrlIn, ServerClient clientIn){
 		stage=stageIn;
 		ctrl=ctrlIn;
 		client=clientIn;
 	}
 	public Scene createScene(){
+		int xBase = 0, yBase = 0;
 		Pane root = new Pane();
-		Label username = new Label();
-		Label subjects = new Label();
-		Label score = new Label();
+		Label username = new Label(); username.setLayoutX(xBase); username.setLayoutY(yBase+50);
+		//Label subjects = new Label(); subjects.setLayoutX(xBase); subjects.setLayoutY(yBase+100);
+		//Label score = new Label(); score.setLayoutX(xBase); score.setLayoutY(yBase+150);
+		//Label classScore = new Label(); score.setLayoutX(xBase); score.setLayoutY(yBase+200);
+		
+		root.getChildren().addAll(username);
 		
 		
-		root.getChildren().addAll(username, subjects, score);
+		updater = new FeedUpdater(client, username, client.ProfileWindow);
+		updater.start();
+		
 		Scene scene = new Scene(root, 1300, 700);
 		
 	return scene;
